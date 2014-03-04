13b10d090b991ce9beeb7170a9a15c66391f1ed2
Samuel Richardson <sam@richardson.co.nz>
2014-03-04 11:16:09 +1100

    Added the ability to configure the color picker through a provider.

--- color-picker.js
+++ color-picker.js
@@ -1,36 +1,52 @@
-angular.module('ngColorPicker', [])
-.directive('ngColorPicker', [function() {
-    var colors = [
-        '#7bd148',
-        '#5484ed', 
-        '#a4bdfc',
-        '#46d6db',
-        '#7ae7bf', 
-        '#51b749',
-        '#fbd75b',
-        '#ffb878', 
-        '#ff887c',
-        '#dc2127',
-        '#dbadff', 
-        '#e1e1e1'
-    ];
-    function controller($scope) {
-        $scope.colors = colors;
-        $scope.selected = $scope.selected || '#e1e1e1';
+var services = angular.module('ngColorPicker.services', [])
+    .provider('ngColorPicker', function () {
 
-        $scope.pick = function(color) {
-            $scope.selected = color;
-        }
-    }
-    function link(scope, element, attr) {
-    }
-    return {
-        scope: {
-            selected: '='
-        },
-        restrict: 'AE',
-        template: '<span ng-repeat="color in colors" ng-class="{selected: (color===selected)}"  ng-click="pick(color)" style="background-color:{{color}};"></span>',
-        controller: controller,
-        link: link
-    }
-}]);
+        var colors,
+            default_colors = [
+                '#7bd148',
+                '#5484ed',
+                '#a4bdfc',
+                '#46d6db',
+                '#7ae7bf',
+                '#51b749',
+                '#fbd75b',
+                '#ffb878',
+                '#ff887c',
+                '#dc2127',
+                '#dbadff',
+                '#e1e1e1'
+            ];
+
+        this.set = function (new_colors) {
+            colors = new_colors;
+        };
+
+        this.$get = function () {
+            return {
+                colors: function () {
+                    return colors || default_colors;
+                }
+            };
+        };
+
+    });
+
+angular.module('ngColorPicker.directives', ['ngColorPicker.services'])
+    .directive('ngColorPicker', function (ngColorPicker) {
+
+        return {
+            scope: {
+                selected: '='
+            },
+            restrict: 'AE',
+            template: '<span ng-repeat="color in colors" ng-class="{selected: (color===selected)}" ng-click="pick(color)" style="background-color:{{color}};"></span>',
+            link: function (scope, element, attr, ngModel) {
+                scope.colors = ngColorPicker.colors();
+                scope.selected = scope.selected || scope.colors[0];
+                scope.pick = function (color) {
+                    scope.selected = color;
+                };
+            }
+        };
+
+    });
